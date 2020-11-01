612b8b7c4d0560fd1bb14e6aacb5c3ef09bb8084
kect3578 <kect3578@gmail.com>
2020-11-01 20:42:07 +0900

    프론트

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  // state (React)
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [movies, setMovies] = useState([
+    { name: "AA", price: 2000 },
+    { name: "BB", price: 3000 },
+  ]);
+
+  const nameOnChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const priceOnChange = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const buttonOnClick = () => {
+    const temp = movies.concat({
+      name: name,
+      price: price,
+    });
+    setMovies(temp);
+  };
+
+  // HTML + CSS
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>영화 리스트</h2>
+      {movies.map((movie) => {
+        return (
+          <div>
+            <ul>
+              <li>영화 제목: {movie.name}</li>
+              <li>영화 가격: {movie.price}</li>
+            </ul>
+          </div>
+        );
+      })}
+      <h2>영화 등록하기!</h2>
+      <div>
+        <span className="box">이름: </span>
+        <input type="text" value={name} onChange={nameOnChange}></input>
+      </div>
+      <div>
+        <span className="box">가격: </span>
+        <input type="number" value={price} onChange={priceOnChange}></input>
+      </div>
+      <button onClick={buttonOnClick}>등록하기</button>
     </div>
   );
 }
